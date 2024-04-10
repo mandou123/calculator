c9ced9fbe9d2f19a877db1af4f79ca43b3dff6fa
developer1 <dev1@email.com>
2024-04-10 15:09:09 +0900

    Add btn2(Clear) and handler function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit)
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox,
+                             QPlainTextEdit, QHBoxLayout)
 from PyQt5.QtGui import QIcon
 
 class Calculator(QWidget):
@@ -15,9 +16,18 @@ class Calculator(QWidget):
         self.btn1 = QPushButton('Message', self) # 버튼 추가
         self.btn1.clicked.connect(self.activateMessage)
 
+        self.btn2 = QPushButton('Clear', self) #버튼 2 추가
+        self.btn2.clicked.connect(self.clearMessage)
+
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(self.btn1)
+        hbox.addWidget(self.btn2)
+
+
         vbox = QVBoxLayout()
         vbox.addWidget(self.te1)
-        vbox.addWidget(self.btn1)
+        vbox.addLayout(hbox)
         vbox.addStretch(1)
 
         self.setLayout(vbox)
@@ -30,6 +40,9 @@ class Calculator(QWidget):
         # 버튼 클릭시에 동작하는 함수
     def activateMessage(self):
         self.te1.appendPlainText("Button clicked!")
+
+    def clearMessage(self):
+        self.te1.clear()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
