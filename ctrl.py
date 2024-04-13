84cee528b2063977eef1051e54c1c81adc08e0bd
developer1 <dev1@email.com>
2024-04-13 15:03:40 +0900

    Modify pow function using exception

--- ctrl.py
+++ ctrl.py
@@ -41,7 +41,10 @@ class Control:
 
     
     def pow(self, a, b):
-        if(a==0):
-            return 0
-        else:
-            return pow(a, b)
+        try:
+            if(a==0):
+                raise Exception("Base Error")
+        except Exception as e:
+            return e
+        
+        return pow(a,b)
